0dd605e300e30279a5b5de34a1b12e3205c34d43
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-12-03 00:25:54 -0500

    implemented steppers can now change border radius of buttons also borders and tint colors of buttons and steppers

--- RandomFeaturesSequel/RandomFeaturesSequel/AMYShapesAndBordersViewController.m
+++ RandomFeaturesSequel/RandomFeaturesSequel/AMYShapesAndBordersViewController.m
@@ -10,6 +10,22 @@
 
 @interface AMYShapesAndBordersViewController ()
 
+@property (weak, nonatomic) IBOutlet UIStepper *widthStepper;
+@property (weak, nonatomic) IBOutlet UIStepper *heightStepper;
+@property (weak, nonatomic) IBOutlet UIButton *clearCornersButton;
+
+@property (weak, nonatomic) IBOutlet UIStepper *borderFrameStepper;
+@property (weak, nonatomic) IBOutlet UIStepper *borderColorStepper;
+@property (weak, nonatomic) IBOutlet UIButton *noBorderButton;
+
+@property (weak, nonatomic) IBOutlet UIStepper *shadowSizeStepper;
+@property (weak, nonatomic) IBOutlet UIStepper *shadowColorStepper;
+@property (weak, nonatomic) IBOutlet UIButton *noShadowButton;
+
+@property (strong, nonatomic) NSArray *buttonsToChange;
+@property (strong, nonatomic) NSArray *steppersToChange;
+@property (strong, nonatomic) NSArray *colorsArray;
+
 @end
 
 @implementation AMYShapesAndBordersViewController
@@ -17,7 +33,161 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self setUpArrayOfColors];
+    
+    self.widthStepper.continuous = YES;
+    self.heightStepper.continuous = YES;
+    self.borderFrameStepper.continuous = YES;
+    self.borderColorStepper.continuous = YES;
+    
+    self.widthStepper.minimumValue = 0;
+    self.widthStepper.maximumValue = self.clearCornersButton.frame.size.width;
+    
+    self.heightStepper.minimumValue = 0;
+    self.heightStepper.maximumValue = self.clearCornersButton.frame.size.height;
+    
+    self.borderFrameStepper.minimumValue = 0;
+    self.borderFrameStepper.maximumValue = 16;
+    
+    self.borderColorStepper.minimumValue = 0;
+    self.borderColorStepper.maximumValue = self.colorsArray.count - 1;
+    
+    self.buttonsToChange = @[ self.clearCornersButton, self.noBorderButton, self.noShadowButton ];
+    
+    self.steppersToChange = @[ self.widthStepper, self.heightStepper, self.borderFrameStepper, self.borderColorStepper, self.shadowSizeStepper, self.shadowColorStepper ];
 }
+
+- (void)setUpArrayOfColors
+{
+    UIColor *brown = [UIColor brownColor];
+    UIColor *magenta = [UIColor magentaColor];
+    UIColor *red = [UIColor redColor];
+    UIColor *orange = [UIColor orangeColor];
+    UIColor *yellow = [UIColor yellowColor];
+    UIColor *green = [UIColor greenColor];
+    UIColor *cyan = [UIColor cyanColor];
+    UIColor *blue = [UIColor blueColor];
+    UIColor *purple = [UIColor purpleColor];
+    UIColor *black = [UIColor blackColor];
+    UIColor *darkGray = [UIColor darkGrayColor];
+    UIColor *lightGray = [UIColor lightGrayColor];
+    UIColor *white = [UIColor whiteColor];
+    
+    self.colorsArray = @[ brown, magenta, red, orange, yellow, green, cyan, blue, purple, black, darkGray, lightGray, white ];
+}
+
+- (void)setStepValue:(CGFloat)currentStepperValue forSender:(UIStepper *)sender
+{
+    CGFloat i = currentStepperValue;
+    
+    if (i < 2)
+    {
+        sender.stepValue = 0.1;
+    }
+    else if (i < 5)
+    {
+        sender.stepValue = 0.3;
+    }
+    else if (i < 8)
+    {
+        sender.stepValue = 0.5;
+    }
+    else if (i < 10)
+    {
+        sender.stepValue = 1;
+    }
+    else
+    {
+        sender.stepValue = 5;
+    }
+}
+
+- (IBAction)widthStepperValueChanged:(UIStepper *)sender
+{
+    CGFloat i = sender.value;
+    [self setStepValue:i forSender:sender];
+    
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.cornerRadius = button.frame.size.width/i;
+    }
+    NSLog(@"stepper value: %f, increment: %f, max: %f", self.widthStepper.value, i, self.clearCornersButton.frame.size.width);
+}
+
+- (IBAction)heightStepperValueChanged:(UIStepper *)sender
+{
+    CGFloat i = sender.value;
+    [self setStepValue:i forSender:sender];
+    
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.cornerRadius = button.frame.size.height/i;
+    }
+}
+
+- (IBAction)clearCornersButtonTapped:(UIButton *)sender
+{
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.cornerRadius = 0;
+    }
+}
+
+- (IBAction)borderFrameStepperValueChanged:(UIStepper *)sender
+{
+    CGFloat i = sender.value;
+    
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.borderWidth = i;
+    }
+}
+
+- (IBAction)borderColorStepperValueChanged:(UIStepper *)sender
+{
+    NSUInteger c = sender.value;
+    
+    UIColor *color = self.colorsArray[c];
+    
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.borderColor = color.CGColor;
+    }
+    for (UIStepper *stepper in self.steppersToChange)
+    {
+        stepper.tintColor = color;
+    }
+}
+
+- (IBAction)noBorderButtonTapped:(UIButton *)sender
+{
+    for (UIButton *button in self.buttonsToChange)
+    {
+        button.layer.borderWidth = 0;
+    }
+    for (UIStepper *stepper in self.steppersToChange)
+    {
+        stepper.tintColor = nil;
+    }
+}
+
+- (IBAction)shadowSizeStepperValueChanged:(UIStepper *)sender
+{
+    
+}
+
+- (IBAction)shadowColorStepperValueChanged:(UIStepper *)sender
+{
+    
+}
+
+- (IBAction)noShadowButtonTapped:(UIButton *)sender
+{
+    
+}
+
+
 
 /*
  so what I would like to do is have a series (maybe 12?) of buttons on the screen.  They should each have an ability--yes, 3 in a row, 4 in a column.  The first should increase something, the second should decrease it (vice versa), and the third should revert it back to normal.
